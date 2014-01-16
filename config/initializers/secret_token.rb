7652447c5363597383b0419bb8a6b8c4098eb123
Michael Elliott <elliotec@Michaels-MacBook-Air.local>
2014-01-16 14:26:20 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foundstore::Application.config.secret_key_base = 'ae3044d158085422d5fffc626509dee4604905b7582dfa31e15d2b7d9ec06a6f79dc3638139809f13055de689c8c4413e47d8b112a0e96a3abddf95306363112'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foundstore::Application.config.session_store :cookie_store, key: '_foundstore_session'
